a1980558282abaacabfec86233a45da190802e6e
Neil <neil@>
2016-01-20 00:04:36 -0800

    Testing adding my lab 3

---
+++ list.cpp
@@ -0,0 +1,71 @@
+#include "list.h"
+
+void duplicate(node* head, node*& newHead)
+{
+	if (head == NULL)
+	{
+		newHead = NULL;
+		return;
+	}
+	
+	newHead = new node;
+	newHead->data = head->data;
+	duplicate(head->next, newHead->next);
+}
+
+int removeTwo(node*& head)
+{
+	node* curr = head;
+	node* prev = NULL;
+
+	if(curr == NULL)
+	{
+		return 0;
+	}
+	else if(curr->data == 2)
+	{
+		if(curr == head)
+		{
+			node* currPtr = head;
+			head = head->next;
+			delete currPtr;
+			currPtr = head;
+			return 1 + removeTwo(head);
+		}
+		else if(curr->next == NULL)
+		{
+			delete curr;
+			curr = NULL;
+			prev->next = NULL;
+			return 1;
+		}
+		else
+		{
+			prev->next = curr->next;
+			delete curr;
+			curr = prev->next;
+			return 1 + removeTwo(curr);
+		}
+	}
+	else
+	{
+		return removeTwo(head->next);
+	}
+	/*DOESNT REMOVE COMPLETELY
+	node* next;
+
+	if(head == NULL)
+	{
+		return 0;
+	}
+	else if(head->data == 2)
+	{
+		next = head->next;
+		delete head;
+		return 1 + removeTwo(next);
+	}
+	else
+	{
+		return removeTwo(head->next);
+	}*/
+}
